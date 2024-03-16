5b322ede18d62a27bbc116ea6f903325d611d175
Bogdan <bogdan.drljevic123@gmail.com>
2024-03-16 08:40:14 +0100

    init

---
+++ HCI_PZ1_PR106-2021/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HCI_PZ1_PR106_2021
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
